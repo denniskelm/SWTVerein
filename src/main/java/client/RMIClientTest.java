8935f36f558e99f43285f0ac1010f1437dfd7567
Gabriel <gabriel.kleebaum@uni-rostock.de>
2022-05-10 23:09:17 +0200

    Interface für Dienstleistungsverwaltung implementiert

--- src/main/java/client/RMIClientTest.java
+++ src/main/java/client/RMIClientTest.java
@@ -18,6 +18,7 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.time.LocalDateTime;
 
 //TODO WAS MACHT DIESE KLASSE?
 public class RMIClientTest {
@@ -36,13 +37,21 @@ public class RMIClientTest {
     }
 
     public static void initializeRMI() throws RemoteException, NotBoundException {
+        //Registry registry = LocateRegistry.getRegistry("meta.informatik.uni-rostock.de", 1234);
         Registry registry = LocateRegistry.getRegistry("127.0.0.1", 1234);
 
         geraeteverwaltung = (IGeraeteverwaltung) registry.lookup("Geraeteverwaltung");
-        //dienstleistungsverwaltung = (IDienstleistungsverwaltung) registry.lookup("Dienstleistungsverwaltung");
+        dienstleistungsverwaltung = (IDienstleistungsverwaltung) registry.lookup("Dienstleistungsverwaltung");
 
-        /* TESTEN
-        geraeteverwaltung.geraetHinzufuegen("Schaufel", "Gabriel", 2, "Schaufeln", "Dies ist eine tolle Schaufel!", "Raum 1");
-        System.out.println(geraeteverwaltung.geraeteDatenAusgeben("0"));    */
+        // ZUM TESTEN
+        /* geraeteverwaltung.geraetHinzufuegen("Schaufel", "Gabriel", 2, "Schaufeln", "Dies ist eine tolle Schaufel!", "Raum 1");
+        System.out.println(geraeteverwaltung.geraeteDatenAusgeben("0")); */
+
+        /* try {
+            dienstleistungsverwaltung.angebotErstellen("Zaun streichen", "Ich biete an, deinen Zaun zu streichen.", "Malern", LocalDateTime.now(), LocalDateTime.of(2022, 5, 26, 15, 10), "Gabriel");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } */
+
     }
 }
--- src/main/java/shared/communication/IDienstleistungsverwaltung.java
+++ src/main/java/shared/communication/IDienstleistungsverwaltung.java
@@ -14,8 +14,18 @@ TODO Dennis Kelm
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.time.LocalDateTime;
 
 // Interface, um RMI für Klasse Dienstleistungsverwaltung zu ermöglichen
 public interface IDienstleistungsverwaltung extends Remote {
-    //TODO: Methoden hinzufügen
+
+    void gesuchErstellen(String titel, String beschreibung, String kategorie, String imageUrl, String ersteller) throws RemoteException, Exception;
+    void angebotErstellen(String titel, String beschreibung, String kategorie, LocalDateTime ab, LocalDateTime bis, String ersteller) throws RemoteException, Exception;
+    void gesuchLoeschen(String gesuchsID) throws RemoteException;
+    void angebotLoeschen(String angebotsID) throws RemoteException;
+    void gesuchAendern(String gesuchsID, Object attr, Object wert) throws RemoteException;
+    void angebotAendern(String angebotsID, Object attr, Object wert) throws RemoteException;
+    void gesuchAnnehmen(String gesuchsID, String ersteller, String nutzer, int stunden) throws RemoteException, Exception;
+    void angebotAnfragen(String angebotsID, String ersteller, String fragender) throws RemoteException;
+
 }
