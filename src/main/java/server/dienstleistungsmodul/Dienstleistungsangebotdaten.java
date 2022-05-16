f553bd52c3e03773da7f52fdf42f9d40220db0d1
Gabriel <gabriel.kleebaum@uni-rostock.de>
2022-05-16 16:30:07 +0200

    Mit Server synchronisiert

---
+++ src/main/java/server/dienstleistungsmodul/Dienstleistungsangebotdaten.java
@@ -0,0 +1,11 @@
+package server.dienstleistungsmodul;
+
+public enum Dienstleistungsangebotdaten {
+    ANGEBOTS_ID,
+    TITEL,
+    BESCHREIBUNG,
+    KATEGORIE,
+    AB,
+    BIS,
+    PERSONEN_ID
+}
