c10006957935b19e506a7d9dc1b0882c25991e3f
Raphael <raphaelkleebaum@yahoo.de>
2022-05-19 09:51:07 +0200

    IRollenverwaltung angepasst

--- src/main/java/shared/communication/IRollenverwaltung.java
+++ src/main/java/shared/communication/IRollenverwaltung.java
@@ -3,12 +3,14 @@ package shared.communication;
 import server.users.Personendaten;
 import server.users.Rolle;
 
+import java.rmi.NoSuchObjectException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.time.LocalDateTime;
 
 /*
 @author
-TODO Raphael Kleebaum
+Raphael Kleebaum
 TODO Jonny Schlutter
 Gabriel Kleebaum
 TODO Mhd Esmail Kanaan
@@ -20,11 +22,15 @@ TODO Dennis Kelm
 
 // Interface, um RMI für Klasse Rollenverwaltung zu ermöglichen
 public interface IRollenverwaltung extends Remote {
+
     Object[] gastListeAnzeigen() throws RemoteException;
     Object[] mitgliedListeAnzeigen() throws RemoteException;
     Object[] mitarbeiterListeAnzeigen() throws RemoteException;
     Object[] vorsitzListeAnzeigen() throws RemoteException;
-    void rolleAendern(String mitgliedsID, Rolle rolle) throws RemoteException;
+    void rolleAendern(String mitgliedsID, Rolle rolle) throws RemoteException, Exception;
     void nutzereintragAendern(String mitgliedsID, Personendaten attr, String wert) throws RemoteException;
     Object[] mahnungsverwaltungAnzeigen() throws RemoteException;
+    void gastHinzufuegen(String nachname, String vorname, String email, String password, String anschrift, String mitgliedsnr, int telefonnummer, boolean spender);
+    boolean login(String email, int password) throws Exception;
+
 }
