e1b8230f23b93ae082459a785c61bd2ec1b94b31
Dennis <email@denniskelm.de>
2022-05-19 17:26:46 +0200

    IDienstleistungsverwaltung sync

--- src/main/java/shared/communication/IDienstleistungsverwaltung.java
+++ src/main/java/shared/communication/IDienstleistungsverwaltung.java
@@ -3,13 +3,13 @@ package shared.communication;
 /*
 @author
 TODO Raphael Kleebaum
-Jonny Schlutter
+TODO Jonny Schlutter
 Gabriel Kleebaum
 TODO Mhd Esmail Kanaan
 TODO Gia Huy Hans Tran
 TODO Ole Björn Adelmann
-Bastian Reichert
-Dennis Kelm
+TODO Bastian Reichert
+TODO Dennis Kelm
 */
 
 import server.dienstleistungsmodul.Dienstleistungsangebotdaten;
@@ -18,29 +18,23 @@ import server.dienstleistungsmodul.Dienstleistungsgesuchdaten;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 
 // Interface, um RMI für Klasse Dienstleistungsverwaltung zu ermöglichen
 public interface IDienstleistungsverwaltung extends Remote {
 
     String gesuchErstellen(String titel, String beschreibung, String kategorie, String imageUrl, String ersteller) throws RemoteException, Exception;
-
     String angebotErstellen(String titel, String beschreibung, String kategorie, LocalDateTime ab, LocalDateTime bis, String ersteller) throws RemoteException, Exception;
-
     void gesuchLoeschen(String gesuchsID) throws RemoteException;
-
     void angebotLoeschen(String angebotsID) throws RemoteException;
-
     void gesuchAendern(String gesuchsID, Dienstleistungsgesuchdaten attr, Object wert) throws RemoteException;
-
     void angebotAendern(String angebotsID, Dienstleistungsangebotdaten attr, Object wert) throws RemoteException;
-
     void gesuchAnnehmen(String gesuchsID, String ersteller, String nutzer, int stunden) throws RemoteException, Exception;
 
     void angebotAnfragen(String angebotsID, String ersteller, String fragender) throws RemoteException;
 
-    Object[] getAngeboteInformationen(String geraeteID) throws RemoteException;
+    void angebotAnnehmen(String gesuchsID, String ersteller, String nutzer, int stunden) throws RemoteException, Exception;
 
+    Object[] getAngeboteInformationen(String geraeteID) throws RemoteException;
     Object[] getGesucheInformationen(String geraeteID) throws RemoteException;
 
 }
