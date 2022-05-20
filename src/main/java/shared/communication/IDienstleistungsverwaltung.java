8d8cf0311d1b1dabe4244561c0bf9fc430b3e34d
Dennis <email@denniskelm.de>
2022-05-20 17:40:17 +0200

    Weiter GUI Verschmelzung - keine Fehler mehr!

--- src/main/java/shared/communication/IDienstleistungsverwaltung.java
+++ src/main/java/shared/communication/IDienstleistungsverwaltung.java
@@ -18,7 +18,6 @@ import server.dienstleistungsmodul.Dienstleistungsgesuchdaten;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 
 // Interface, um RMI für Klasse Dienstleistungsverwaltung zu ermöglichen
 public interface IDienstleistungsverwaltung extends Remote {
@@ -27,10 +26,15 @@ public interface IDienstleistungsverwaltung extends Remote {
     String gesuchErstellen(String titel, String beschreibung, String kategorie, String imageUrl, String ersteller) throws RemoteException, Exception;
     String angebotErstellen(String titel, String beschreibung, String kategorie, LocalDateTime ab, LocalDateTime bis, String ersteller) throws RemoteException, Exception;
     void gesuchLoeschen(String gesuchsID) throws RemoteException;
+
     void angebotLoeschen(String angebotsID) throws RemoteException;
+
     void gesuchAendern(String gesuchsID, Dienstleistungsgesuchdaten attr, Object wert) throws RemoteException;
+
     void angebotAendern(String angebotsID, Dienstleistungsangebotdaten attr, Object wert) throws RemoteException;
+
     void gesuchAnnehmen(String gesuchsID, String ersteller, String nutzer, int stunden) throws RemoteException, Exception;
+
     void angebotAnnehmen(String gesuchsID, String ersteller, String nutzer, int stunden) throws RemoteException, Exception;
     Object[] getAngeboteInformationen(String geraeteID) throws RemoteException; //Object[]
     Object[] getGesucheInformationen(String geraeteID) throws RemoteException;  //Object[]
