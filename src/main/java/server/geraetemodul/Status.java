a9847ace4cdd2e3a556bb4c87f7cf80a0cafa82d
Raphael <raphaelkleebaum@yahoo.de>
2022-05-22 13:12:42 +0200

    Status vereinfacht

--- src/main/java/server/geraetemodul/Status.java
+++ src/main/java/server/geraetemodul/Status.java
@@ -1,18 +1,8 @@
 package server.geraetemodul;
 
 public enum Status {
-    FREI("Frei"),
-    BEANSPRUCHT("Beansprucht"),
-    AUSGELIEHEN("Ausgeliehen");
-
-    String name;
-
-    Status(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
+    FREI,
+    BEANSPRUCHT,
+    AUSGELIEHEN
 
 }
