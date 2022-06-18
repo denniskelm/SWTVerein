55ac2f5e622e7194fc4c9db29b397f7d410f2868
Ole <ole.adelmann@gmx.net>
2022-06-18 16:01:47 +0200

    IRollenverwaltung Methode hinzugefügt

--- src/main/java/shared/communication/IRollenverwaltung.java
+++ src/main/java/shared/communication/IRollenverwaltung.java
@@ -17,28 +17,15 @@ Gabriel Kleebaum
 // Interface, um RMI für Klasse Rollenverwaltung zu ermöglichen
 public interface IRollenverwaltung extends Remote {
 
-    Object[] gastListeAnzeigen() throws RemoteException;    //Object[]
-
-    Object[] mitgliedListeAnzeigen() throws RemoteException;    //Object[]
-
-    Object[] mitarbeiterListeAnzeigen() throws RemoteException; //Object[]
-
-    Object[] vorsitzListeAnzeigen() throws RemoteException; //Object[]
 
     void rolleAendern(String mitgliedsID, Rolle rolle) throws RemoteException, Exception;
 
     void nutzereintragAendern(String mitgliedsID, Personendaten attr, String wert) throws RemoteException;
 
-    Object[] mahnungsverwaltungAnzeigen() throws RemoteException;   //Object[]
-
     Object[] gastHinzufuegen(String nachname, String vorname, String email, String password, String anschrift, String mitgliedsnr, String telefonnummer, boolean spender) throws RemoteException;
 
     Object[] login(String email, String password) throws Exception;
 
-    //ArrayList<Gast> getGaeste() throws RemoteException;  //ArrayList<Gast>
-    //ArrayList<Mitglied> getMitglieder() throws RemoteException;  //ArrayList<Mitglied>
-    //ArrayList<Mitarbeiter> getMitarbeiter() throws RemoteException; //ArrayList<Mitarbeiter>
-    //ArrayList<Vorsitz> getVorsitze() throws RemoteException;    //ArrayList<Vorsitz>
     long getIdCounter() throws RemoteException;
 
     String getMitgliedsNamen(String MitgliedsID) throws RemoteException, Exception;
@@ -66,9 +53,9 @@ public interface IRollenverwaltung extends Remote {
     public Object[][] vorsitzDaten() throws RemoteException;
 
     public Object[][] mitarbeiterDaten() throws RemoteException;
-
     public Object[][] gaesteDaten() throws RemoteException;
-
     public Object[] gastDaten(String mitgliedsID) throws RemoteException;
+    public String mahnungsIdErstellen() throws RemoteException;
+
 
 }
