b7052b3eb81e788198f789345d4b6e00ebcc5037
Dennis <email@denniskelm.de>
2022-06-18 18:49:39 +0200

    Abschlussmeeting - 17.06.2022

--- src/main/java/client/gui/Profilseite/Profilseite.java
+++ src/main/java/client/gui/Profilseite/Profilseite.java
@@ -204,7 +204,7 @@ public class Profilseite {
                     "-",
                     "-",
                     "-",
-                    "-",
+                    "-"
             });
         }
 
